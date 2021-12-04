72ff839a1ae1f9dd233a830398928d40c15f0b06
Carl Greenstreet <cwgstreet@gmail.com>
2021-12-04 11:56:00 +1030

    start menu library

---
+++ cloudSmoker_uno/lib/menu/menu.cpp
@@ -0,0 +1,18 @@
+/* ***************************************************************
+* menu.h - lcd menu library
+* 
+*  C W Greenstreet, Ver1, 4Dec21
+*    MIT Licence - Released into the public domain
+*
+** ************************************************************* */
+/* 
+// include 3rd party libraries
+#include <Arduino.h>
+#include <NewEncoder.h>
+#include <Wire.h>                           // i2C device communication
+#include <hd44780.h>                        // LCD library
+#include <hd44780ioClass/hd44780_I2Cexp.h>  // i2c expander i/o class header -> required for my YwRobot 1602 LCD
+ */
+//incliude local libraries
+#include <menu.h>
+
---
+++ cloudSmoker_uno/lib/menu/menu.h
@@ -0,0 +1,55 @@
+/* ***************************************************************
+* menu.h - lcd menu library
+* 
+*  C W Greenstreet, Ver1, 4Dec21
+*    MIT Licence - Released into the public domain
+*
+** ************************************************************* */
+
+#ifndef LCD_H
+#define LCD_H
+
+#if ARDUINO >= 100  // this if-else block manages depreciated versions of Arduino IDE
+#include <Arduino.h>
+#else
+#include <WConstants.h>
+#include <WProgram.h>
+#include <pins_arduino.h>
+#endif  // end if-block
+
+/******************************************************
+// Libraries required for periphial function tests:
+*******************************************************/
+
+// i2C devices
+//#include <Wire.h>
+
+//YwRobot 1602 LCD with i2c i/o exapander backpack (PCF8574 or MCP23008)
+//#include <hd44780.h>
+//#include <hd44780ioClass/hd44780_I2Cexp.h>  // i2c expander i/o class header -> required for my YwRobot 1602 LCD
+
+
+/******************************************************
+// Class definitions for lcd functions and tests:
+*******************************************************/
+/* 
+class CWG_LCD {
+   public:
+    CWG_LCD(const int lcdCols, const int lcdRows);  //constructor - will initialise lcdCols, lcdRows
+
+    void functionTest();
+    void initialiseCustomCharSet();
+
+   private:
+    int _numCols;
+    int _numRows;
+};
+ */
+/******************************************************
+// Helper function prototype:
+*******************************************************/
+
+//void gotoRowCol(int thisRow, int thatCol);  //move into class?
+
+
+#endif  // end header guard
